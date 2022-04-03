ae48c618c28e72f4bfaf54ce8441a7f08d60a79d
D-eshan <dixitesh@msu.edu>
2022-04-03 17:23:07 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+anime({
+  targets: 'div.box.r1',
+  translateY: [
+    {value: 100, duration: 1200},
+    {value:0, duration: 800}
+  ],
+  rotate:{
+    value: '1turn',
+    easing: 'easeInOutSine'
+  }
+});
